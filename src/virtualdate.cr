a0ffb08eabe57740c4eedd750523d410d935dc0a
Davor Ocelic <docelic@crystallabs.io>
2025-12-23 01:17:01 +0100

    Bump to v1.2.0

--- src/virtualdate.cr
+++ src/virtualdate.cr
@@ -3,7 +3,7 @@ require "virtualtime"
 # VirtualDate builds on VirtualTime to represent due/omit rules plus higher-level scheduling semantics.
 class VirtualDate
   VERSION_MAJOR    = 1
-  VERSION_MINOR    = 1
+  VERSION_MINOR    = 2
   VERSION_REVISION = 0
   VERSION          = [VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION].join '.'
 
